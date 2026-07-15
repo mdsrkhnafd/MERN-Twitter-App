2ea5bd042ef28670226c370d55dc3d5602c67e12
Mudasir Khan <mudasir.khan@riphah.edu.pk>
2026-07-15 11:40:33 +0500

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // Import Modules
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
@@ -23,10 +24,11 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json({ limit: "5mb" })); // limit should be greater than the size of the image to prevent DOS attack
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 // app.use(
 //   cors({
 //     origin: "http://localhost:5173",
@@ -36,16 +38,20 @@ app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cors());
 app.use(cookieParser());
 
-// Root Route
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
-
 // API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+// Serve frontend build files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // Start Server
 const startServer = async () => {
